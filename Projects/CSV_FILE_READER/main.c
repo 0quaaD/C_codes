7fe16705b970822f295b55161829ae8c3913bf33
Elnar Ismayilov <109694568+0quaaD@users.noreply.github.com>
2025-05-04 14:22:34 +0900

    Update main.c

--- Projects/CSV_FILE_READER/main.c
+++ Projects/CSV_FILE_READER/main.c
@@ -1,23 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "csv_reader.h"
-void print_feature_name(){
-    printf("Index |  FEATURE_1 | FEATURE_2 | FEATURE_3 | FEATURE_4 | FEATURE_5 | FEATURE_6 | FEATURE_7 | FEATURE_8 | FEATURE_9 | TARGET\n");
-}
+#include <time.h>
+
 int main(void)
 {
     float X[ROWS][COLS-1], y[ROWS];
     printf("\n========== THE CSV FILE CONTENTS ==========\n\n");
-    print_feature_name();
-    int row_ = load_file("/home/elnokin/Downloads/classification_data.csv",X,y);
+    printf("index");
+    clock_t start = clock();
+    headers_name("./classification_data.csv");
+    puts("");
+    int row_ = load_file("./classification_data.csv",X,y);
     if(row_ < 1) return -1;
     for(int i=0;i<row_;++i){
-        printf("%d | ",i+1);
+        printf("%d\t",i+1);
         for(int j=0;j<COLS-1;++j){
-            printf(" %f ",X[i][j]);
+            printf("%f\t\t",X[i][j]);
         }
-        printf("--> %.1f\n",y[i]);
+        printf("%.1f\n",y[i]);
         if(i != row_ -1) printf("------------------------------------------------------------------------------------------------------\n");
+        clock_t end = clock();
+        double spent_time = (double)(end-start)/CLOCKS_PER_SEC;
+        printf("\n%lf seconds took to run the program.\n",spent_time);
     }
 
     return 0;
