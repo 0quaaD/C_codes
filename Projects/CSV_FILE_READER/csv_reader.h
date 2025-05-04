45a868eea147e88b3e0e4412b4cd0869746b8c25
Elnar Ismayilov <109694568+0quaaD@users.noreply.github.com>
2025-05-04 14:28:40 +0900

    Update csv_reader.h

--- Projects/CSV_FILE_READER/csv_reader.h
+++ Projects/CSV_FILE_READER/csv_reader.h
@@ -5,10 +5,11 @@
 #define ROWS 200
 #define COLS 10
 #define LINE_LEN 1024
+#define FIELD_LEN 50
 
 int load_file(const char* filename, float X[ROWS][COLS-1], float y[ROWS]);
 int count_rows(const char* filename);
 int count_cols(const char* filename);
-char feature_name(const char* filename);
+void headers_name(const char* filename);
 
 #endif
